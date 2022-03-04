c54bcfdcdb2d41c53aad0c313419ba0f75b1d610
Connor White <connorwhitepdx@gmail.com>
2022-03-04 00:38:58 -0800

    ignition and acc switches established

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const five = require('johnny-five');
-
 const board = new five.Board();
+
+const acc = new five.Switch;
+const ignition = new five.Switch;
+
+const sensorA4 = new five.Sensor();
 
 board.on("ready", () => {
     const led = new five.Led(3);
     led.blink(250);
 });
 
-const sensorA4 = new five.Sensor();
+new five.Thermometer({
+    pin: 10
+});
+
+new five.Pin({
+    pin: 12
+});
